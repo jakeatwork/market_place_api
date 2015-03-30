bf270026f7841730583e6571ebd84f87d74c002c
Jake Levine <jake@agileramen.com>
2015-03-30 10:30:09 -0700

    Finish auth logic and clean up tests

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -5,7 +5,8 @@ class Authentication
 end
 
 describe Authenticable do
-  let(:authentication) { Authentication.new }
+  let(:authentication) { Authentication.new } 
+  subject { authentication }
 
   describe "#current_user" do
     before do
@@ -32,5 +33,25 @@ describe Authenticable do
     end
 
     it {  should respond_with 401 }
+  end
+
+  describe "#user_signed_in?" do
+    context "when there is a user on 'session'" do
+      before do
+        @user = FactoryGirl.create :user
+        authentication.stub(:current_user).and_return(@user)
+      end
+
+      it { should be_user_signed_in }
+    end
+
+    context "when there is no user on 'session'" do
+      before do
+        @user = FactoryGirl.create :user
+        authentication.stub(:current_user).and_return(nil)
+      end
+
+      it { should_not be_user_signed_in }
+    end
   end
 end

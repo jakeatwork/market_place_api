104cd72d81ccbd6e77f7f6077135291dc6a840ef
Jake Levine <jake@agileramen.com>
2015-03-30 15:49:22 -0700

    Get product searializer tests passing

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -9,7 +9,7 @@ describe Api::V1::ProductsController do
     end
 
     it "returns the information about a reporter on a hash" do
-      product_response = json_response
+      product_response = json_response[:product]
       expect(product_response[:title]).to eql @product.title
     end
 
@@ -18,13 +18,13 @@ describe Api::V1::ProductsController do
 
   describe "GET #index" do
     before(:each) do
-      5.times { FactoryGirl.create :product } 
+      4.times { FactoryGirl.create :product } 
       get :index
     end
 
-    it "returns 1 record from the database" do
+    it "returns 4 records from the database" do
       products_response = json_response
-      expect(products_response.size).to eq(5)
+      expect(products_response[:products].size).to eq(4)
     end
 
     it { should respond_with 200 }
@@ -40,7 +40,7 @@ describe Api::V1::ProductsController do
       end
 
       it "renders the json representation for the product record just created" do
-        product_response = json_response
+        product_response = json_response[:product]
         expect(product_response[:title]).to eql @product_attributes[:title]
       end
 
@@ -82,7 +82,7 @@ describe Api::V1::ProductsController do
       end
 
       it "renders the json representation for the updated user" do
-        product_response = json_response
+        product_response = json_response[:product]
         expect(product_response[:title]).to eql "An expensive TV"
       end
 

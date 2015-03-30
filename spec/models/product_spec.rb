cf305acef2cc8a160ebfc5c51aebdb2e20c84b70
Jake Levine <jake@agileramen.com>
2015-03-30 13:01:25 -0700

    Add dependent destroy on products association with user

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,6 +8,7 @@ describe Product do
   it { should respond_to(:price) }
   it { should respond_to(:published) }
   it { should respond_to(:user_id) }
+  it { should belong_to :user }
 
   # it { should not_be_published }
 

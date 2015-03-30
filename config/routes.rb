0e3ba5da0d5c289fab502b6a8ee00298158a4b0c
Jake Levine <jake@agileramen.com>
2015-03-30 00:06:30 -0700

    Fix rest of the inet issue and cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -3,52 +3,6 @@ require 'api_constraints'
 MarketPlaceApi::Application.routes.draw do
   mount SabisuRails::Engine => "/sabisu_rails"
 
-  # mount ApiTaster::Engine => "/api_taster" if Rails.env.development?
-
-  # ApiTaster.routes do
-
-  #   get '/users/sign_in', {}
-
-  #   post '/users/sign_in', {}
-
-  #   delete '/users/sign_out', {}
-
-  #   post '/users/password', {}
-
-  #   get '/users/password/new', {}
-
-  #   get '/users/password/edit', {}
-
-  #   patch '/users/password', {}
-
-  #   put '/users/password', {}
-
-  #   get '/users/cancel', {}
-
-  #   post '/users', {}
-
-  #   get '/users/sign_up', {}
-
-  #   get '/users/edit', {}
-
-  #   patch '/users', {}
-
-  #   put '/users', {}
-
-  #   delete '/users', {}
-
-  #   post '/users', {}
-
-  #   get '/users/:id', {}
-
-  #   patch '/users/:id', {}
-
-  #   put '/users/:id', {}
-
-  #   delete '/users/:id', {}
-
-  # end
-
   devise_for :users
   # Api definition
   namespace :api, defaults: { format: :json },
